a520ee2df2227415946e7e331d2f6ee5c4781787
johanna domzalski <jojodom4@gmail.com>
2024-09-29 12:25:13 +0200

    ajout javasript récupération photo et infos photographes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,15 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// photographer.js
+export function photographerFactory(data) {
+  const { id, name, city, tagline, price, portrait } = data;
+  const article = document.createElement('article');
+
+  article.innerHTML = `
+      <img src="assets/photographers/${portrait}" alt="${name}">
+      <h2>${name}</h2>
+      <p>${city}</p>
+      <p>${tagline}</p>
+      <p>${price}€/jour</p>
+  `;
+
+  return article;
+}
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,3 +1,5 @@
+import { displayModal, closeModal } from './utils/contactForm.js';
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
